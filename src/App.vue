53284967ece3867a94f633516f712849ac4c6b90
FirinKinuo <deals@fkinuo.ru>
2021-09-14 12:30:08 +0300

    UPDATE: restructuring project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <body id="app">
+  <noscript>
+    <strong>JavaScript must be enabled for modern web))</strong>
+  </noscript>
+  <router-view/>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "src/assets/scss/variables";
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
+
+:root {
+  font-size: 20px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  outline: none;
+  color: $dark;
+  border: 0;
+  scrollbar-width: none;
+}
+
+*::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/layouts/main-layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-layout">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-main-layout"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
