ba222ca0085e8d9361dd78ed707c6ddd27675276
FirinKinuo <deals@fkinuo.ru>
2021-09-18 15:57:14 +0300

    ADD: Component of the button for selecting a category on the welcome screen

---
+++ src/components/category-button/index.vue
@@ -0,0 +1,72 @@
+`<template>
+  <router-link to="/" class="category-button">
+    <simple-svg
+        :src="imported_svg_path"
+        custom-class-name="category-button__icon"
+    />
+    <span class="category-button__name">
+      {{ primary_text }} <span :style="secondary_text_style">{{ secondary_text }}</span>
+    </span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "v-category-button",
+  props: {
+    primary_text: {
+      String,
+      require: true
+    },
+    secondary_text: {
+      String
+    },
+    secondary_text_color: {
+      String
+    },
+    icon_name: {
+      String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      secondary_text_style: `color:${this.secondary_text_color}`,
+      imported_svg_path: require(`@/assets/images/svg/${this.icon_name}.svg`)
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/scss/variables";
+
+.category-button {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  gap: 20px;
+  padding: 0 40px;
+  width: 435px;
+  height: 300px;
+  background: linear-gradient(300deg, #9D62E8 30%, #E94C5E 100%);
+  border-radius: $border-radius;
+  box-sizing: border-box;
+  transition: 200ms ease-in;
+
+  &__name {
+    color: #fff;
+    font-size: 2.2rem;
+    font-weight: bold;
+  }
+
+  ::v-deep & {
+    &__icon {
+      width: 100px !important;
+      height: 100px !important;
+      fill: white;
+    }
+  }
+}
+</style>`
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import {SimpleSVG} from 'vue-simple-svg'
+Vue.component('simple-svg', SimpleSVG)
 
 Vue.config.productionTip = false
 
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
